c8c5bb54a1433334da4dc932a77f5cf2e140877f
Estefania <estefi.95.c@gmail.com>
2019-05-20 16:41:41 +0200

    Recomendador de libros hechos. Guardar libros en listas predefinidas.

--- usuario/views.py
+++ usuario/views.py
@@ -6,9 +6,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ExtendedUserCreationForm, UsuarioForm, EstadoForm
 from .models import Usuario, LibroUsuario
 from libro.models import Genero, Libro
+import random
 # Create your views here.
 
-#Bienvenida
+
+# Bienvenida
 def bienvenida(request):
     if request.user.is_authenticated:
         user = request.user
@@ -21,7 +23,7 @@ def bienvenida(request):
     return render(request, 'bienvenida/bienvenida.html', {})
 
 
-#Logout
+# Logout
 def salir(request):
     logout(request)
     return render(request, 'bienvenida/bienvenida.html')
@@ -31,40 +33,84 @@ def salir(request):
 def home(request):
     user = request.user
     usuario = Usuario.objects.get(usuario=user)
-    #Todos los generos para mostrarlos en la home
-    generos = Genero.objects.all()
-    #Todos los libros ordenados por mas nuevos
-    libros = Libro.objects.all().order_by('-anoEdicion')
+    # Todos los generos para mostrarlos en la home
+    generosTodos = Genero.objects.all()
+    # Todos los libros ordenados por mas nuevos
+    librosNuevos = Libro.objects.all().order_by('-anoEdicion')[:5]
+
+# Libros recomendados
+    # Array para generos de libros recomendados
     generosLibrosUsuario = []
-#Libros recomendados
-    #Seleccionar generos de los libros que el usuario ha guardado como leído.
-    try:
-        leidos = LibroUsuario.objects.filter(usuario=usuario, estado="LD")
-
-        for libroU in leidos:
-            libroUser = libroU.libro
-            print(libroUser.genero)
-            generosLibrosUsuario.append(libroUser.genero)
-
-    except:
-        libroUsuario = None
-    #Seleccionar libros de los generos favoritos
-    librosUser = []
     contador = 0
+    # Seleccion generos
+    while contador < 5:
+        # Seleccionar generos de los libros que el usuario ha guardado como leído.
+        try:
+            # Seleccionar los generos que el usuario ha guardado como favoritos
+            if contador <= 5:
+                generos = usuario.genero.all()
+                for genero in generos:
+                    if contador >= 5:
+                        break
+                    generosLibrosUsuario.append(genero.nombre)
+                    contador = contador + 1
 
+            leidos = LibroUsuario.objects.filter(usuario=usuario, estado="LD")
 
+            for libroU in leidos:
+                if contador >= 5:
+                    break
+                libroUser = libroU.libro
+                generos = libroUser.genero.all()
+                for genero in generos:
+                    generosLibrosUsuario.append(genero.nombre)
+                    contador = contador + 1
 
-    context = {'generos': generos, 'libros': libros}
+        except:
+            libroUsuario = None
+
+        # Si no tiene ningun libro ni genero añadido en favoritos seleccionará 5 generos
+        if contador == 0:
+            generos = Genero.objects.all()[:5]
+            for genero in generos:
+                if contador >= 5:
+                    break
+                generosLibrosUsuario.append(genero.nombre)
+                contador = contador + 1
+
+    # Seleccionar libros de los generos favoritos
+    librosUser = []
+    for genero in generosLibrosUsuario:
+        generoLibro = Genero.objects.get(nombre=genero)
+        libroU = Libro.objects.filter(genero=generoLibro)
+        libroAleatorio = random.choice(libroU)
+        if libroAleatorio not in librosUser:
+            librosUser.append(libroAleatorio)
+
+    librosRecomendados = []
+    contador=0
+    for libro in librosUser:
+        libroRecom = Libro.objects.get(titulo=libro)
+        print(libroRecom)
+        contador = contador + 1
+        librosRecomendados.append(libroRecom)
+
+    context = {'generos': generosTodos, 'librosNuevos': librosNuevos, 'librosRecomendados': librosRecomendados}
     return render(request, 'web/home.html', context)
 
 
 def libro(request, id_libro):
     libro = get_object_or_404(Libro, pk=id_libro)
+    user = request.user
+    usuario=Usuario.objects.get(usuario=user)
     form = EstadoForm()
     if request.method == 'POST':
-        form = EstadoForm()
-        print(form)
+        form = EstadoForm(request.POST)
 
+        if form.is_valid():
+            valor = form.cleaned_data['estado']
+
+            libroUsuario = LibroUsuario.objects.create(usuario=usuario, libro=libro, estado=valor)
 
     context = {'libro': libro, 'form': form}
 
@@ -94,7 +140,7 @@ def registro(request):
     else:
         form = ExtendedUserCreationForm()
 
-    context = {'form': form }
+    context = {'form': form}
 
     return render(request, 'registro/registro.html', context)
 
@@ -127,7 +173,7 @@ def miPerfil(request):
     user = request.user
     usuario = Usuario.objects.get(usuario=user)
     generosUser = usuario.genero.all()
-
+    # Añadir generos al usuaio
     if request.method == 'POST':
         generos = request.POST.getlist('genero')
         usuario.genero.clear()
@@ -139,7 +185,19 @@ def miPerfil(request):
         return render(request, 'prueba.html', context)
 
     generos = Genero.objects.all()
+
+    # Libros leídos del usuario
     leidos = LibroUsuario.objects.filter(usuario=usuario, estado="LD")
 
-    context = {'generos': generos, 'generoUser': generosUser, 'usuario': usuario, 'leidos': leidos}
+    # Libros leer del usuario
+    leer = LibroUsuario.objects.filter(usuario=usuario, estado="LR")
+    print(leer)
+
+    # Libros comprar del usuario
+    comprar = LibroUsuario.objects.filter(usuario=usuario, estado="CM")
+
+    # Libros abandonados del usuario
+    abandonados = LibroUsuario.objects.filter(usuario=usuario, estado="AB")
+
+    context = {'generos': generos, 'generoUser': generosUser, 'usuario': usuario, 'leidos': leidos, 'leer': leer, 'comprar': comprar, 'abandonados': abandonados}
     return render(request, 'web/perfil.html', context)
